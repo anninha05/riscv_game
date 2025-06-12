0f64db6dc2f1e6b61fdd159ba6c794d7c7c170d4
iarloo <iarlohenrique5@gmail.com>
2025-06-12 16:40:58 -0300

    Sprites e assets adicionados

---
+++ Sprites/game_assets.s
@@ -0,0 +1,3 @@
+.include "/home/iarlo/Área de trabalho/projeto/Jogo da velha/Sprites/assets/asset_tile.s"
+.include "/home/iarlo/Área de trabalho/projeto/Jogo da velha/Sprites/assets/asset_O_simbolo.s"
+.include "/home/iarlo/Área de trabalho/projeto/Jogo da velha/Sprites/assets/asset_X_simbolo.s"
