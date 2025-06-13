201fa9320b0c00becb7f2f3f15b855a9dc9f1f05
iarloo <iarlohenrique5@gmail.com>
2025-06-13 19:31:39 -0300

    removido sprites, jogo pronto, falta polir agora!

--- Sprites/game_assets.s
+++
@@ -1,3 +0,0 @@
-.include "/home/iarlo/Área de trabalho/projeto/Jogo da velha/Sprites/assets/asset_tile.s"
-.include "/home/iarlo/Área de trabalho/projeto/Jogo da velha/Sprites/assets/asset_O_simbolo.s"
-.include "/home/iarlo/Área de trabalho/projeto/Jogo da velha/Sprites/assets/asset_X_simbolo.s"
